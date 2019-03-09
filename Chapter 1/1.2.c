fdeb49d4a46fcfca060957f6e1ab254f6ce07771
Teo\nirvana_rsc <nirvana_rsc@yahoo.com>
2019-03-09 21:09:58 +0900

    Chapter 1 Problem 2

--- Chapter 1/1.2.c
+++ Chapter 1/1.2.c
@@ -1,34 +1,42 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
-void bin(uint8_t n) {
-  for (uint8_t i = 1 << 7; i > 0; i = i / 2)
-    (n & i) ? printf("1") : printf("0");
+char* sortString(char* string) {
+  char temp;
+
+  int i, j;
+  int n = strlen(string);
+
+  for (i = 0; i < n - 1; i++) {
+    for (j = i + 1; j < n; j++) {
+      if (string[i] > string[j]) {
+        temp = string[i];
+        string[i] = string[j];
+        string[j] = temp;
+      }
+    }
+  }
+  return string;
 }
 
-int main() {
-  printf("Test1\n");
-  printf("Test2\n");
+int main(int argc, char** argv) {
+  if (argc < 3) {
+    printf("%s expects at least 2 arguments\n", argv[0]);
+    return 1;
+  }
 
-  // int c = 0b1100011;
-  uint8_t c = 257;
-  uint8_t b = 2 << c;
-  uint8_t a = 1 << c;
-  uint8_t z = 1 << 7;
-  uint8_t z_and = 5 & 3;
-  uint8_t z_or = 5 | 3;
-  printf("%d\t%d\t%d\n", c, b, a);
-  printf("%d\n", z);
-  bin(5);
-  printf("\n");
-  bin(3);
-  printf("\n");
-  bin(40);
-  printf("\n");
-  printf("%d\n", z_and);
-  printf("%d\n", z_or);
-  bin(z_and);
-  printf("\n");
-  bin(z_or);
-  printf("\n");
+  char* a = argv[1];
+  char* b = argv[2];
+
+  sortString(a);
+  sortString(b);
+
+  for (uint8_t i = 0; i < strlen(a); i++) {
+    if (a[i] != b[i]) {
+      printf("Strings are not a permutation of each other\n");
+      return 0;
+    }
+  }
+  printf("Strings are a permutation of each other\n");
 }
